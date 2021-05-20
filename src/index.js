6dac30331827c4e752b021874ce241d5bd59ea39
Adnan Veseli <adnan.veseli@gmail.com>
2021-05-20 09:25:10 +0200

    simulating behaviour

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// The DOM node to observe
+const observableTarget = document.body;
+
+// Callback function when changes occurs
+function mutationCallback(mutations, observer) {
+  console.log("mutation record", mutations);
+
+  // TODO iterate mutations and node list and search for the react root node!
+  // when found, do the rendering.
+  const rootElem = document.getElementById("root");
+  if (rootElem) {
+    renderReactApp(rootElem);
+  }
+}
+
+// Create a new instance of MutationObserver with callback in params
+const observer = new MutationObserver(mutationCallback);
+
+// Setup config
+const config = {
+  childList: true,
+  subtree: true,
+};
+
+// When everything is ready, we just observe our target
+observer.observe(observableTarget, config);
+
+function renderReactApp(elem) {
+  const rootElem = document.getElementById("root");
+
+  if (rootElem) {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      elem
+    );
+  }
+}
+
+window.renderReactApp = renderReactApp;
+
+// renderReactApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
